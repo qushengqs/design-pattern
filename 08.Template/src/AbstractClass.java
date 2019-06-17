bdf8b8089b0f6de9dbe164d7c12d68e1dcfee9b1
曲圣 <qusheng@qushengdeMacBook-Pro.local>
2019-06-17 22:33:41 +0800

    模板模式

---
+++ 08.Template/src/AbstractClass.java
@@ -0,0 +1,10 @@
+public abstract class AbstractClass {
+
+    public abstract void primitiveOperation1();
+    public abstract void primitiveOperation2();
+
+    public void templateMethod(){
+        this.primitiveOperation1();
+        this.primitiveOperation2();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
         factoryTest();
         prototypeTest();
         prototypeDeepTest();
+        templateTest();
     }
 
     /**
@@ -121,26 +122,41 @@ public class Main {
     /**
      * 原型模式--深拷贝
      */
-    public static void prototypeDeepTest(){
-        try
-        {
+    public static void prototypeDeepTest() {
+        try {
             System.out.println("原型模式--深拷贝");
-            ResumeDeep resumeDeep=new ResumeDeep("大鸟");
-            resumeDeep.setPersonInfo("男","29");
-            resumeDeep.setWorkExperience("1998-2000","XX公司");
+            ResumeDeep resumeDeep = new ResumeDeep("大鸟");
+            resumeDeep.setPersonInfo("男", "29");
+            resumeDeep.setWorkExperience("1998-2000", "XX公司");
 
-            ResumeDeep resumeDeepB=(ResumeDeep)resumeDeep.clone();
-            resumeDeepB.setWorkExperience("1998-2006","YY企业");
+            ResumeDeep resumeDeepB = (ResumeDeep) resumeDeep.clone();
+            resumeDeepB.setWorkExperience("1998-2006", "YY企业");
 
-            ResumeDeep resumeDeepC=(ResumeDeep)resumeDeep.clone();
-            resumeDeepC.setPersonInfo("男","24");
-            resumeDeepC.setWorkExperience("1998-2003","ZZ企业");
+            ResumeDeep resumeDeepC = (ResumeDeep) resumeDeep.clone();
+            resumeDeepC.setPersonInfo("男", "24");
+            resumeDeepC.setWorkExperience("1998-2003", "ZZ企业");
 
             resumeDeep.display();
-            resumeDeepB.display();;
+            resumeDeepB.display();
+            ;
             resumeDeepC.display();
-        }catch (CloneNotSupportedException ex ){
+        } catch (CloneNotSupportedException ex) {
             System.out.println(ex);
         }
+    }
+
+    /**
+     * 模板模式
+     */
+    public static void templateTest() {
+        System.out.println("模板模式");
+
+        AbstractClass abstractClass;
+
+        abstractClass = new ConcreteClassA();
+        abstractClass.templateMethod();
+
+        abstractClass = new ConcreteClassB();
+        abstractClass.templateMethod();
     }
 }
